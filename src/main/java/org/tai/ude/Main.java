10f73b1d0dcea86024c2ca8e3724d348182fd90d
TheAlchemyIndex <adam.vaughan@hotmail.co.uk>
2023-11-25 19:47:35 +0000

    removing data and joiners

--- src/main/java/org/tai/ude/Main.java
+++ src/main/java/org/tai/ude/Main.java
@@ -1,7 +1,6 @@
 package org.tai.ude;
 
 import org.tai.ude.config.UnderstatConfig;
-import org.tai.ude.joiners.UnderstatJoiner;
 import org.tai.ude.understat.UnderstatExtractor;
 import org.tai.ude.writers.FileWriter;
 
@@ -10,9 +9,6 @@ public class Main {
     public static void main(String[] args) {
         UnderstatConfig config = new UnderstatConfig("src/main/resources/config.properties");
         final String mainSeason = config.getMainSeason();
-        final int startingSeasonStart = config.getStartingSeasonStart();
-        final int startingSeasonEnd = config.getStartingSeasonEnd();
-        final int finalSeasonEnd = config.getFinalSeasonEnd();
         final String baseFilePath = config.getBaseFilePath();
         final String mainUrl = config.getMainUrl();
         final String playerUrl = config.getPlayerUrl();
@@ -22,9 +18,5 @@ public class Main {
         UnderstatExtractor understatExtractor = new UnderstatExtractor(mainSeason, mainUrl, playerUrl, fileWriter);
         understatExtractor.getTeamData();
         understatExtractor.getPlayerData();
-
-        UnderstatJoiner understatJoiner = new UnderstatJoiner(startingSeasonStart, startingSeasonEnd, finalSeasonEnd, fileWriter);
-        understatJoiner.joinPlayerData();
-        understatJoiner.joinTeamData();
     }
 }
